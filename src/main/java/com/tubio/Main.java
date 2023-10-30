84e4ff9a6522545529f8dff64b643d09fffd5d5d
Adrian <adrian.tubio@outlook.com>
2023-10-30 17:57:02 +0800

    add receiving connection functionality for websocket

--- src/main/java/com/tubio/Main.java
+++ src/main/java/com/tubio/Main.java
@@ -1,7 +1,55 @@
 package com.tubio;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final String WEBSOCKET_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        ServerSocket server = new ServerSocket(80);
+
+        try {
+            System.out.println("Server has started on localhost:80");
+            Socket client = server.accept();
+            System.out.println("A client connected.");
+
+            InputStream in = client.getInputStream();
+            OutputStream out = client.getOutputStream();
+
+            Scanner s = new Scanner(in, StandardCharsets.UTF_8);
+            String data = s.useDelimiter("\\r\\n\\r\\n").next();
+            Matcher get = Pattern.compile("^GET").matcher(data);
+
+            if (get.find()) {
+                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+                match.find();
+
+                byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n" +
+                        "Connection: Upgrade\r\n" +
+                        "Upgrade: websocket\r\n" +
+                        "Sec-WebSocket-Accept: " +
+                        Base64.getEncoder()
+                                .encodeToString(MessageDigest
+                                        .getInstance("SHA-1")
+                                        .digest((match.group(1) + WEBSOCKET_GUID).getBytes(StandardCharsets.UTF_8)))
+                        + "\r\n\r\n").getBytes(StandardCharsets.UTF_8);
+
+                out.write(response, 0, response.length);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
